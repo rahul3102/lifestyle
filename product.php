7c10e7c407a9f7fd5a41f0b6d0ec98fec3e20cb0
rahul3102 <106545152+rahul3102@users.noreply.github.com>
2022-05-30 21:02:09 +0530

    Update product.php

--- product.php
+++ product.php
@@ -20,7 +20,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a href="index.html" class="navbar-brand">Lifestyle Store</a>
+                <a href="index.php" class="navbar-brand">Lifestyle Store</a>
             </div>
             
             <div class="collaspe navbar-collapse" id="mynavbar">
