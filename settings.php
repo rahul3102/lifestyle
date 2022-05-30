97d6ab845ee0582c8258ea74de6cf542563b2005
rahul3102 <106545152+rahul3102@users.noreply.github.com>
2022-05-30 21:02:52 +0530

    Update settings.php

--- settings.php
+++ settings.php
@@ -21,7 +21,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="index.html" class="navbar-brand">Lifestyle Store</a>
+          <a href="index.php" class="navbar-brand">Lifestyle Store</a>
           
         </div>
           
